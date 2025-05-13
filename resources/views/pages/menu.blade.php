0e55b700ad1001a2dca978986c0cbec0be38f3fd
Angelica167 <angelicajoliepratamas@gmail.com>
2025-05-13 21:26:12 +0700

    Angelica:memperbaiki bagian pages menu,cart,dan history

--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Menuuu| Kuliner Sumbar</title>
+    <title>Menu| Kuliner Sumbar</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
     <link href="{{ asset('build/assets/app.css') }}" rel="stylesheet">
@@ -14,7 +14,7 @@
 
     <!-- Content -->
     <div class="max-w-7xl mx-auto mt-22 px-4">
-        <h2 class="text-3xl font-bold text-center mb-10 text-gray-800">Daftar Menuuu</h2>
+        <h2 class="text-3xl font-bold text-center mb-10 text-gray-800">Daftar Menu</h2>
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             @foreach($menus as $menu)
